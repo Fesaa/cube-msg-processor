a9d85ecc7aae8ad08d0dcb9f58ad8b067bd592a9
Fesaa <77553571+Fesaa@users.noreply.github.com>
2022-06-02 21:41:49 +0200

    Rework ConsecutiveTime
    - Tracks several staff members
    - Better variable naming
    - Ordered ConsecutiveTime graph
    - Removed trailing whitespaces
    - Use of datetime.timedelta

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
-from init import options
+import csv
+import time
+import json
+import asyncio
+import warnings
+import requests
 import matplotlib.pyplot as plt
-from datetime import datetime
+from datetime import datetime, timedelta
 from colorama import Fore, Style
-import requests, csv, json, asyncio, time, warnings
 
-config = json.load(open('config.json'))
-TOKEN =config.get('TOKEN')
+from init import options
 
-id_cache = dict()
-id_load_time = list()
+config = json.load(open('config.json', encoding='utf-8'))
+TOKEN = config.get('TOKEN')
 
-async def get_name_from_id(id: int) -> str:
+ID_CACHE = {}
+ID_LOAD_TIME = []
+
+
+async def get_name_from_id(user_id: int) -> str:
     start = time.time()
 
-    if id in id_cache.keys():
-        return id_cache[id]
+    if user_id in ID_CACHE:
+        return ID_CACHE[user_id]
     else:
         while True:
-            res = requests.get(f"https://discord.com/api/v9/users/{id}", headers={"Authorization": TOKEN})
+            res = requests.get(f"https://discord.com/api/v9/users/{user_id}", headers={"Authorization": TOKEN})
 
             if str(res) == "<Response [429]>":
                 time_out = res.json()['retry_after']
@@ -30,16 +37,20 @@ async def get_name_from_id(id: int) -> str:
                 if len(username) > 15:
                     username = username[:15] + '...'
 
-                id_cache[id] = username
-                id_load_time.append(time.time() - start)
+                ID_CACHE[user_id] = username
+                ID_LOAD_TIME.append(time.time() - start)
+
                 return username
 
             else:
                 print(res)
                 await asyncio.sleep(1)
 
+
 async def correct_dict_for_id(d: dict) -> dict:
-    return {await get_name_from_id(int(key)) if key.isdigit() else key : value for key, value in d.items()}
+    return {await get_name_from_id(int(key)) if key.isdigit() else key: value for key, value in d.items()}
+
+delta = timedelta(microseconds=1)
 
 async def main():
     total_run_time = time.time()
@@ -55,13 +66,13 @@ async def main():
 
     if args['ConsecutiveTime']:
         dictConsecutiveTime = {}
-    
+
     if args['ReplyTimes']:
-        dictReplyTimes = {i: [datetime(year=2022, month=10, day=1, microsecond=1) - datetime(year=2022, month=10, day=1)] for i in range(24)}
+        dictReplyTimes = {i: [delta] for i in range(24)}
 
     if args['DailyStaffMessages']:
         dictDayStaffMessages = {}
-    
+
     file_reading_time = time.time()
 
     for index, FileName in enumerate(FileNames):
@@ -75,25 +86,20 @@ async def main():
 
             if index == 0:
                 if args['ConsecutiveTime']:
-                    current_staff = 0
-                    current_count = 0
-                    current_time = start_time
-                    interruptions = 5
-                    ConsecutiveTime_non_staff_replies = 0
-                
+                    active_staff_members = {}
+
                 if args['ReplyTimes']:
                     last_reply_time = start_time
                     non_staff_replies = 0
-                
+
                 if args['StartDate'] == 'First Date':
                     start_date = str(start_time.date())
                 else:
                     start_date = args['StartDate']
-                
 
             for row in [first_row] + list(reader):
                 last_time = datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S.%f')
-            
+
                 if start_date <= str(last_time.date()):
 
                     if args['TotalMessages']:
@@ -115,41 +121,46 @@ async def main():
                                 dictDayMessages[day] = 1
 
                     if args['ConsecutiveTime']:
+
+                        to_remove = []
+
                         if row[1] != 'non staff replied':
-                            if row[1] != current_staff:
-                                if current_count > 5:
-                                    if interruptions == 5:
-                                        if current_staff != 0:
-                                            if current_staff in dictConsecutiveTime:
-                                                dictConsecutiveTime[current_staff].append(datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S.%f') - current_time)
-                                            else:
-                                                dictConsecutiveTime[current_staff] = [datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S.%f') - current_time]
-                                        current_staff, current_count, current_time, interruptions = row[1], 1, datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S.%f'), 0
-                                    else:
-                                        interruptions += 1
+                            if row[1] not in active_staff_members:
+                                active_staff_members[row[1]] = {'replies': 0, 'interruptions': 0, 'start_time': last_time, 'last_time': last_time}
+
+                            for staff_user_id, info in active_staff_members.items():
+                                if row[1] == staff_user_id:
+                                    info['replies'] += 1
+                                    info['interruptions'] = 0
+                                    info['last_time'] = last_time
+
                                 else:
-                                    current_staff, current_count, current_time, interruptions = row[1], 1, datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S.%f'), 0
-                            else:
-                                current_count += 1
-                                interruptions = 0
+                                    info['interruptions'] += 1
+
+                                    if info['interruptions'] == 5:
+                                        if staff_user_id in dictConsecutiveTime:
+                                            dictConsecutiveTime[staff_user_id].append(info['last_time'] - info['start_time'])
+                                        else:
+                                            dictConsecutiveTime[staff_user_id] = [info['last_time']- info['start_time']]
+                                        
+                                        to_remove.append(staff_user_id)
+
                         else:
-                            if (datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S.%f') - current_time).total_seconds()/60 > 10 and current_staff != 0:
-                                timedelta = datetime(year=1, month=1, day=1, minute=10) - datetime(year=1, month=1, day=1, minute=0)
-                                if current_staff in dictConsecutiveTime:
-                                    dictConsecutiveTime[current_staff].append(timedelta)
-                                else:
-                                    dictConsecutiveTime[current_staff] = [timedelta]
-                            elif ConsecutiveTime_non_staff_replies == 20:
-                                if current_staff != 0:
-                                    if current_staff in dictConsecutiveTime:
-                                        dictConsecutiveTime[current_staff].append(datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S.%f') - current_time)
-                                    else:
-                                        dictConsecutiveTime[current_staff] = [datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S.%f') - current_time]
-                                current_staff, current_count, current_time, interruptions = 0, 0, datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S.%f'), 0
-                            else:
-                                ConsecutiveTime_non_staff_replies += 1
 
+                            for staff_user_id, info in active_staff_members.items():
+                                time_difference: timedelta = last_time - info['last_time']
 
+                                if time_difference.total_seconds()/60 > 10:
+                                    if info['replies'] > 5:
+                                        if staff_user_id in dictConsecutiveTime:
+                                            dictConsecutiveTime[staff_user_id].append(info['last_time'] - info['start_time'] + timedelta(minutes=10))
+                                        else:
+                                            dictConsecutiveTime[staff_user_id] = [info['last_time'] - info['start_time'] + timedelta(minutes=10)]
+
+                                    to_remove.append(staff_user_id)
+                            
+                        for staff_user_id in to_remove:
+                            active_staff_members.pop(staff_user_id)
 
                     if args['ReplyTimes']:
 
@@ -161,7 +172,7 @@ async def main():
                             if non_staff_replies > args['IgnoreMessages']:
                                 dictReplyTimes[last_time.hour].append(last_time - last_reply_time)
                             non_staff_replies, last_reply_time = 0, last_time
-                    
+
                     if args['DailyStaffMessages']:
                         if row[1] != "non staff replied":
                             if row[1] in dictDayStaffMessages:
@@ -171,7 +182,6 @@ async def main():
                                     dictDayStaffMessages[row[1]][last_time.day] = 1
                             else:
                                 dictDayStaffMessages[row[1]] = {last_time.day: 1}
-                            
 
     file_reading_time_end = time.time()
     processing_dicts_time = time.time()
@@ -197,7 +207,6 @@ async def main():
         plt.bar(l0, l1)
         plt.title('Amount of msg per member')
         plt.xticks(l0, l0, rotation='vertical')
-        
 
     if args['Daily']:
 
@@ -212,22 +221,25 @@ async def main():
         plt.xticks(l0, l0, rotation='vertical')
 
     if args['ConsecutiveTime']:
+        dictConsecutiveTime = {key: sum([i.total_seconds()/(3600) for i in value]) for key, value in dictConsecutiveTime.items()}
         dictConsecutiveTime = await correct_dict_for_id(dictConsecutiveTime)
         dictConsecutiveTime = dict(sorted(dictConsecutiveTime.items(), key=lambda item: item[1], reverse=True))
 
-        print(f'{Fore.MAGENTA}ConsecutiveTime{Style.RESET_ALL}:\n'+"".join([i.ljust(30) if (index + 1) % 2 != 0 else i + '\n' for index, i in enumerate(f"{key}: {round(sum([i.total_seconds()/(3600) for i in value]), 2)}" for key, value in dictConsecutiveTime.items())]))
+        print(f'{Fore.MAGENTA}ConsecutiveTime{Style.RESET_ALL}:\n'+"".join([i.ljust(30) if (index + 1) % 2 != 0 else i + '\n' for index, i in enumerate(
+            f"{key}: {round(value, 2)}" for key, value in dictConsecutiveTime.items())]))
 
-        l0 = [key for key in dictConsecutiveTime.keys() if sum([i.total_seconds()/(3600) for i in dictConsecutiveTime[key]]) > 0.5]
-        l1 = [sum([i.total_seconds()/(3600) for i in dictConsecutiveTime[key]]) for key in l0]
+        l0 = [key for key, value in dictConsecutiveTime.items() if value > 0.5]
+        l1 = [dictConsecutiveTime[key] for key in l0]
         plt.subplot(*graph_placements[current_index])
         current_index += 1
         plt.bar(l0, l1)
         plt.title('Total time spend in hours')
         plt.xticks(l0, l0, rotation='vertical')
-    
+
     if args['ReplyTimes']:
 
-        print(f'{Fore.MAGENTA}ReplyTimes{Style.RESET_ALL}:\n'+"".join([i.ljust(30) if (index + 1) % 2 != 0 else i + '\n' for index, i in enumerate(f"{key}: {round(sum([i.total_seconds()/(60) for i in value])/len(value), 2)}" for key, value in dictReplyTimes.items())]))
+        print(f'{Fore.MAGENTA}ReplyTimes{Style.RESET_ALL}:\n'+"".join([i.ljust(30) if (index + 1) % 2 != 0 else i + '\n' for index, i in enumerate(
+            f"{key}: {round(sum([i.total_seconds()/(60) for i in value])/len(value), 2)}" for key, value in dictReplyTimes.items())]))
 
         l0 = [str(i) for i in dictReplyTimes.keys()]
         l1 = [sum([j.total_seconds()/60 for j in i])/len(i) for i in dictReplyTimes.values()]
@@ -236,7 +248,7 @@ async def main():
         plt.bar(l0, l1)
         plt.title(f'Avarage wait time on a question for a given hour. Ignores {args["IgnoreMessages"]} messages')
         plt.xticks(l0, l0, rotation='vertical')
-    
+
     if args['DailyStaffMessages']:
         dictDayStaffMessages = await correct_dict_for_id(dictDayStaffMessages)
 
@@ -248,7 +260,6 @@ async def main():
         plt.title(f'Avarage msg per member on a day')
         plt.xticks(l0, l0, rotation='vertical')
 
-
     plt.subplots_adjust(left=0.1, bottom=0.15, right=0.9, top=0.9, wspace=0.2, hspace=0.75)
     plt.suptitle(f'Data from {start_date} until {last_time.date()}')
 
@@ -257,7 +268,7 @@ async def main():
     print(f"--- Total Run Time {time.time() - total_run_time} seconds ---")
     print(f"--- File Reading Time {file_reading_time_end - file_reading_time} seconds ---")
     print(f"--- Making Plots Time {processing_dicts_time_end - processing_dicts_time} seconds ---")
-    print(f"--- Fetching ids {sum(id_load_time)} seconds ---")
+    print(f"--- Fetching ids {sum(ID_LOAD_TIME)} seconds ---")
 
     if args['SaveGraphs']:
         plt.savefig('out.png')
